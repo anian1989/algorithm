16bd7a2eefb200a31bab09a2a03cee5fe0d9a0b7
junshuaizhang1 <junshuaizhang@163.com>
2017-03-21 17:50:46 +0800

    随机数数组

--- src/Common/RandomUnit.java
+++ src/Common/RandomUnit.java
@@ -1,5 +1,6 @@
 package Common;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import org.junit.Test;
@@ -19,7 +20,6 @@ public class RandomUnit {
 	 * @Title: rangOne
 	 * @Description:返回[-rang,rang]之间的随机数
 	 * @param rang
-	 * @return
 	 * @return: int
 	 */
 	public int rangOne(int rang){
@@ -39,7 +39,6 @@ public class RandomUnit {
 	 * @Title: negativeAndpositive
 	 * @Description: 生成[0,|rang|]之间的随机数，根据rang的正负，返回正负
 	 * @param rang 可为正数负数
-	 * @return
 	 * @return: int
 	 */
 	public int negativeAndpositive(int rang){
@@ -48,5 +47,83 @@ public class RandomUnit {
 		int random = (int)(Math.random()*abs);
 		return random*abs/rang;		
 	}
-
+	
+	/**
+	 * 
+	 * @Title: getFlag
+	 * @Description: 返回一个0或者1的标志位
+	 * @return: int
+	 */
+	public int getFlag(){
+		int flag = (int)(Math.random()*1000);//正负标志位
+		flag = flag%2;
+		return flag;
+	}
+	/**
+	 * 
+	 * @Title: minAndMax
+	 * @Description: 生成[min,max]之间的数据
+	 * @param min
+	 * @param max
+	 * @return: int
+	 */
+	
+	public int minAndMax(int min,int max){
+		if(min >= max){
+			return 0;
+		}
+		if(min <0&& max >0){
+			if(this.getFlag()==1){
+				return this.negativeAndpositive(max);
+			}else
+				return this.negativeAndpositive(min);
+		}
+		return this.negativeAndpositive(max-min)+min;
+		
+	}
+	/**
+	 * @Title: getArrays
+	 * @Description: 获取一个长度是length的范围在[min,max]之间的随机数序列，乱序
+	 * @param length
+	 * @param min
+	 * @param max
+	 * @return: int[]
+	 */
+	public int[] getArrays(int length,int min,int max){
+		if(length<1){
+			return null;
+		}
+		int[] arrays = new int[length];
+		for (int i=0 ; i<arrays.length;i++) {
+			arrays[i] = this.minAndMax(min, max);
+		}
+		return arrays;
+	}
+	/**
+	 * @Title: getArrysNotRepeat
+	 * @Description: 获取一个不重复的随机数数组
+	 * @param length
+	 * @param min
+	 * @param max
+	 * @return: int[]
+	 */
+	public int[] getArrysNotRepeat(int length,int min,int max){
+		if(length<1||length>(max-min)){
+			return null;
+		}
+		int[] arrays = new int[length];
+		int i=0;
+		while(i !=arrays.length) {
+			int key = this.minAndMax(min, max);
+			int binarySearch = Arrays.binarySearch(arrays, key);
+			if(binarySearch <0){
+				arrays[i] = key;
+				i++;
+			}
+			
+		}
+		return arrays;
+	}
+	
+	
 }
--- src/Test/TestCommon.java
+++ src/Test/TestCommon.java
@@ -8,9 +8,15 @@ public class TestCommon {
 	@Test
 	public void testRondom(){
 		RandomUnit randomUnit = new RandomUnit();
-		for (int i = 0; i < 100; i++) {
+		/*for (int i = 0; i < 100; i++) {
 			
-			System.out.println(randomUnit.rangOne(2000));
+			System.out.println(randomUnit.minAndMax(0,100));
+		}*/
+		long currentTimeMillis = System.currentTimeMillis();
+		int[] arrays = randomUnit.getArrysNotRepeat(100000, -10, 100000);
+		System.out.println("Ê±¼ä"+(System.currentTimeMillis()-currentTimeMillis));
+		for (int i = 0; i < arrays.length; i++) {
+			System.out.println(arrays[i]);
 		}
 	}
 }
