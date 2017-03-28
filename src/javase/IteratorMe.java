fcf7a31428ceb601dfa15f8fdf4f4a731d110aff
junshuaizhang1 <junshuaizhang@163.com>
2017-03-28 11:01:40 +0800

    Iterator

---
+++ src/javase/IteratorMe.java
@@ -0,0 +1,16 @@
+package javase;
+
+import java.util.ArrayList;
+/**
+ * 
+ * @ClassName: IteratorMe
+ * @Description: Iterator接口的实现是在各个Collection里面，用私有内部类实现
+ * @author: junshuaizhang1
+ * @date: 2017年3月28日 上午10:56:28
+ */
+public class IteratorMe {
+
+	public void diceng(){
+		ArrayList arrayList = new ArrayList();
+	}
+}
